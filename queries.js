dbb950aa3dfe5d647e3ef30a78efbdbd49988a88
Chantee Smith <chantee.smith@gmail.com>
2020-01-06 23:29:37 -0600

    commit

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ const pool = new Pool ({
     host: 'localhost',
     database: 'remedies',
     password: '',
-    port: 5432,
+    port: process.env.PORT || 5432,
 })
 
 const getUsers = (request, response) => { 
